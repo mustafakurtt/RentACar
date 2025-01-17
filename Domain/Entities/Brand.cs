61327b6a2cbc39bba199496d40fde09326cac329
Mustafa Kurt <mstfkurt07@gmail.com>
2025-01-17 12:26:18 +0300

    Add project files.

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public Brand()
+    {
+        
+    }
+
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
